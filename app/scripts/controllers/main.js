44c2ac16c430e195d6c8f43ff2d572db7f1ca2ea
Francisco Martinez Espartero <francisco.mar.esp@gmail.com>
2014-09-25 20:36:34 +0200

    primer commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gdgsevillagithubioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gdgsevillagithubioApp
+ */
+angular.module('gdgsevillagithubioApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
