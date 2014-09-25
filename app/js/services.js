4e01f99f740e61be813098cf934c364fe4c1271c
Francisco Martinez Espartero <francisco.mar.esp@gmail.com>
2014-09-25 20:12:40 +0200

    adding a readme so i can add this bracnh to my repo on github

--- app/js/services.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-define(["angular", "angular-resource"], function(angular){
-    /* Services Module */
-
-    var services = angular.module('services', ['ngResource']);
-
-    return services;
-})
